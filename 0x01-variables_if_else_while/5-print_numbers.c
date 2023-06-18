b173f213bb9127c6b03f7dc2ea7c435e84731dfd
KellenPretorius <kellenpretorius117@gmail.com>
2023-06-18 16:57:54 -0700

    Print numbers 0 through 10 but only single digits. Tried using put and printf

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Print numbers of base 10 with single digits
+ *
+ * Return: Always 0 (Success)
+ *
+ */
+
+int main(void)
+{
+	int num;
+
+	for (num = 0; num < 10; num++)
+	{
+		printf("%d", num);
+	}
+	putchar('\n');
+	return (0);
+}
