611fe44c24b28d36da5f27879b77f2c9a7e00e9a
KellenPretorius <kellenpretorius117@gmail.com>
2023-06-18 17:33:27 -0700

    Task 8. Print base 16 in lower

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - print base16 in lower case
+ *
+ * Return: Always 0 (Success)
+ *
+ */
+
+int main(void)
+{
+	int base;
+	char alpha;
+
+	for (base = '0'; base < '9'; base++)
+	{
+		putchar(base);
+	}
+	for (alpha = 'a'; alpha <= 'f'; alpha++)
+	{
+		putchar(alpha);
+	}
+	putchar('\n');
+	return (0);
+}
