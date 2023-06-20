04c436d8bcc6d128b9b1b77f9fbe2bc4bb5cbf34
KellenPretorius <kellenpretorius117@gmail.com>
2023-06-20 13:25:47 -0700

    Bettified

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -3,11 +3,15 @@
 
 /**
  * main - print_alphabet - print all alphabet in lowercase
+ *
+ *Return: none
+ *
  */
 
 void print_alphabet(void)
 {
 	char letter;
+
 	for (letter = 'a'; letter <= 'z'; letter++)
 	{
 		_putchar(letter);
