0406c896ef53d93b8c0ed06d4f6b42d5d933d38b
KellenPretorius <kellenpretorius117@gmail.com>
2023-06-22 16:44:44 -0700

    More betty issues

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,10 +1,14 @@
 #include "main.h"
 
 /**
-* _isupper - checks for uppercase character
-* @c: Variable text
-* Return: Always 0.
-*/
+ *
+ * _isupper - checks for uppercase character
+ *
+ * @c: Variable text
+ *
+ * Return: Always 0.
+ *
+ */
 
 int _isupper(int c)
 {
